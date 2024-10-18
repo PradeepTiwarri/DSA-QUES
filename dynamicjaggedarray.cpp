edb4c2fc3f174cceaf7d3709db37f3f42e9c2deb
PradeepTiwarri <pradeeptiwarii731@gmail.com>
2024-10-18 21:36:42 +0530

    Creating a dynamic jagged 2d array

---
+++ dynamicjaggedarray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row;
+    cin >> row;
+    int col;
+    int **arr = new int *[row];
+    int sizes[5];
+    for (int i = 0; i < row; i++)
+    {
+        cout << "enter the number of clumn in " << i + 1 << endl;
+        cin >> col;
+        sizes[i] = col;
+
+        for (int j = 0; j < sizes[i]; j++)
+        {
+            arr[i] = new int[col];
+        }
+    }
+
+    for (int j = 0; j < row; j++)
+    {
+        for (int i = 0; i < sizes[j]; i++)
+        {
+            cout << "enter the elements row wise for row" << j + 1 << endl;
+            cin >> arr[j][i];
+            cout << "press ENTER" << endl;
+        }
+    }
+
+    for (int j = 0; j < row; j++)
+    {
+        for (int i = 0; i < sizes[j]; i++)
+        {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
